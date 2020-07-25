61f04c26ee8a96e482ac103a98c7109fdb26bd6c
doniwirawan <doniwirawan166@gmail.com>
2020-07-25 20:01:33 +0800

    adding some effect and some section to file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function showMore(){
-    alert('hello world')
-}
+$('#btnToggle').click(function(){
+    $('.hidden').toggle(1200);
+})
+$('#contactUs').click(function(){
+    $('.hiddens').toggle(1200);
+})
