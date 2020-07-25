ead70a60fa280e9760de65d302a71f0832fafb28
doniwirawan <doniwirawan166@gmail.com>
2020-07-25 19:13:13 +0800

    deleting hidden items

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 function showMore(){
-    var x = document.getElementsById('hidden');
-    if(x.className === 'hidden'){
-        x.className += ' show';
-    }else{
-        x.className  = 'hidden';
-    }
+    alert('hello world')
 }
